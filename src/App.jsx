1152844f466197f86b539d7de52c42b71923a9e1
Adegbola Timothy <adegbolatimothyjosh@gmail.com>
2026-06-26 09:06:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import NavBar from "./components/NavBar.jsx";
+import HeroSection from "./sections/HeroSection.jsx";
+import gsap from "gsap";
+import {ScrollTrigger, ScrollSmoother} from "gsap/all";
+import MessageSection from "./sections/MessageSection.jsx";
+import FlavorSection from "./sections/FlavorSection.jsx";
+import {useGSAP} from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const App = () => {
+
+    useGSAP(() => {
+        ScrollSmoother.create({
+            smooth: 3,
+            effects: true,
+        })
+    })
+    return (
+        <main>
+            <NavBar />
+            <div id="smooth-wrapper">
+                <div id="smooth-content">
+                    <HeroSection />
+                    <MessageSection />
+                    <FlavorSection />
+                    <div className="h-dvh border border-red-500"/>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default App;
